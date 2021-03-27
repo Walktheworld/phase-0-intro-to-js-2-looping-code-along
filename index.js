088b0b447dafb67212222d1a8be51b7bb274f598
Joe Brown <walktheworld@me.com>
2021-03-26 23:51:59 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+
+const cards = []
+
+function writeCards(array) {
+    for (let i = 0; i < array.length; i++){
+        cards. push (`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return cards;
+}
+
+
+
+
+
+
+
+let integer = []
+function countDown(integer){
+    while (integer >= 0) {
+        console.log(integer--);
+    }
+}
